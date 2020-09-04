532ff4eb20a3be3cd4f24f9353db36fb100c05c8
waller-d <28006978+waller-d@users.noreply.github.com>
2020-09-04 12:27:45 -0400

    Updated code to include calculation of SAT math and SAT ERW for all students who have test scores.

--- test-score-conversion.R
+++ test-score-conversion.R
@@ -1,96 +1,93 @@
-##############################
-####################  waller-d
-#
-#
-#   Test score conversion
-#  
-#
-#
-####################
-##############################
+# Title -------------------------------------------------------------------
+
+#  Test score conversion
+
+
+# Packages ----------------------------------------------------------------
 
 library(readxl)
 library(tidyverse)
-library(psych)
-library(PerformanceAnalytics)
 
+
+# Import concordance table data -------------------------------------------
 
 # Import concordance tables for ACT to new SAT and old SAT to new SAT
-ACT_to_SAT_new <- read_excel("SAT_ACT_concordance.xlsx", 
-                                  sheet = "ACT-composite-to-SAT-total")
-SAT_old_WCR_to_SAT_new_EBRW <- read_excel("SAT_ACT_concordance.xlsx", 
-                                 sheet = "SAT-old-W+CR-to-SAT-new-EBR+W")
 SAT_old_M_to_SAT_new_M <- read_excel("SAT_ACT_concordance.xlsx", 
-                                 sheet = "SAT-old-M-to-SAT-new-M")
+                                     sheet = "SAT-old-M-to-SAT-new-M")
+SAT_old_WCR_to_SAT_new_EBRW <- read_excel("SAT_ACT_concordance.xlsx", 
+                                          sheet = "SAT-old-W+CR-to-SAT-new-EBR+W")
+ACT_math_to_SAT_math <- read_excel("SAT_ACT_concordance.xlsx", 
+                                   sheet = "ACT-M-to-SAT-M")
+ACT_ER_to_SAT_new_ERW <- read_excel("SAT_ACT_concordance.xlsx", 
+                                   sheet = "ACT-E+R-to-SAT-ERW")
+ACT_to_SAT_new <- read_excel("SAT_ACT_concordance.xlsx", 
+                             sheet = "ACT-composite-to-SAT-total")
 
-# Filter for first-time full-time students and select columns of interest
+
+# Create data frames for analysis ------------------------------------------
+
+# Filter for test score columns
 students_test_scores <- students %>%
-  select(hashed_puid, citizenship_desc, profile_academic_period, 
-         profile_highest_act_composite, profile_sat_total, profile_highest_sat_crit_read, 
-         profile_highest_sat_math, profile_highest_sat_writing)
+  select(hashed_puid, profile_highest_sat_math, profile_highest_sat_writing,
+         profile_highest_sat_crit_read, profile_highest_act_english, profile_highest_act_math,
+         profile_highest_act_reading, profile_highest_act_composite, profile_sat_total)
 
 # Rename ACT composite and SAT total columns for merging purposes
-colnames(students_test_scores) <- c("hashed_puid", "citizenship_desc", "profile_academic_period", 
-                             "ACT_composite", "SAT_old", "profile_highest_sat_crit_read", 
-                             "SAT_old_Math", "profile_highest_sat_writing")
+colnames(students_test_scores) <- c("hashed_puid", "SAT_math_old", "SAT_writing_old", "SAT_crit_read_old",
+                                    "ACT_english", "ACT_math", "ACT_read", "ACT_total", "SAT_total_old")
 
 # Rename SAT total column for merging purposes
-colnames(ACT_to_SAT_new) <- c("ACT_composite", "SAT_new", 
+colnames(ACT_to_SAT_new) <- c("ACT_total", "SAT_total_new", 
                               "SAT_total_min", "SAT_total_max")
+colnames(ACT_math_to_SAT_math) <- c("ACT_math", "SAT_math_new")
+colnames(ACT_ER_to_SAT_new_ERW) <- c("ACT_E+R", "SAT_ERW_new", "single_score_ACT_E+R")
+colnames(SAT_old_M_to_SAT_new_M) <- c("SAT_math_old", "SAT_math_new", "SAT_math_test")
+colnames(SAT_old_WCR_to_SAT_new_EBRW) <- c("SAT_W+CR_old", "SAT_ERW_new")
+
+
+# Convert old SAT scores to new SAT scores --------------------------------
 
 # Calculate combined critical reading and writing score
 # Convert old SAT critical reading and writing score to new SAT evidence-based reading and writing score
 # Convert old SAT math score to new SAT math score
 # Combine new SAT evidence-based reading and writing score with new SAT math score for new SAT total score
 SAT_new <- students_test_scores %>% 
-  mutate("SAT_old_W+CR" = profile_highest_sat_crit_read + profile_highest_sat_writing) %>% 
-  filter(!is.na(SAT_old)) %>% # Filter for students who took the SAT
-  left_join(SAT_old_WCR_to_SAT_new_EBRW, by="SAT_old_W+CR") %>% 
-  left_join(SAT_old_M_to_SAT_new_M, by="SAT_old_Math") %>% 
-  mutate("SAT_new"=`SAT_new_EBR+W`+`SAT_new_Math`) %>% 
-  select(-SAT_new_Math_test,-`SAT_new_EBR+W`,-`SAT_new_Math`,-`SAT_old_W+CR`)
+  filter(!is.na(SAT_total_old)) %>% # Filter for students who took the SAT
+  mutate("SAT_W+CR_old" = SAT_writing_old + SAT_crit_read_old) %>% 
+  left_join(SAT_old_WCR_to_SAT_new_EBRW, by = "SAT_W+CR_old") %>% 
+  left_join(SAT_old_M_to_SAT_new_M, by = "SAT_math_old") %>%
+  mutate("SAT_total_new" = SAT_ERW_new + SAT_math_new) %>% 
+  select(hashed_puid, SAT_ERW_new, SAT_math_new, SAT_total_new)
 
-# Convert ACT scores to new SAT scores
+
+# Convert ACT scores to new SAT scores ------------------------------------
+
+# Calculate combined English and reading score
+# Convert combined English and reading ACT score to evidence-based reading and writing SAT score
+# Convert math ACT score to math SAT score
 ACT_all <- students_test_scores %>% 
-  filter(is.na(SAT_old) & !is.na(ACT_composite)) %>% # Filter for students who only took the ACT
-  left_join(ACT_to_SAT_new, by="ACT_composite") %>% 
-  select(-SAT_total_min, -SAT_total_max)
+  filter(is.na(SAT_total_old) & !is.na(ACT_total)) %>% # Filter for students who only took the ACT
+  mutate("ACT_E+R" = ACT_english + ACT_read) %>% 
+  left_join(ACT_ER_to_SAT_new_ERW, by = "ACT_E+R") %>% 
+  left_join(ACT_math_to_SAT_math, by = "ACT_math") %>% 
+  mutate("SAT_total_new" = SAT_ERW_new + SAT_math_new) %>% 
+  select(hashed_puid, SAT_ERW_new, SAT_math_new, SAT_total_new)
+
+
+# Combine test score data frames ------------------------------------------
 
 # Filter for students who did not take either the ACT or SAT
 no_test_all <- students_test_scores %>% 
-  filter(is.na(SAT_old) & is.na(ACT_composite)) %>% 
-  mutate(SAT_new=SAT_old)
+  filter(is.na(SAT_total_old) & is.na(ACT_total)) %>% 
+  mutate(SAT_ERW_new = SAT_writing_old, SAT_math_new = SAT_math_old, SAT_total_new = SAT_total_old) %>% 
+  select(hashed_puid, SAT_ERW_new, SAT_math_new, SAT_total_new)
 
 # Check to see if all students are accounted for in the test score conversion (looking for TRUE)
 # Combine data frames with concordance scores
 (nrow(SAT_new)+nrow(ACT_all)+nrow(no_test_all))==nrow(students_test_scores)
 students_test_scores_equated <- rbind(SAT_new, ACT_all, no_test_all)
 
-# Add coloumn to student data with equated concordance scores
-concordance_scores <- students_test_scores_equated %>% 
-  select(hashed_puid, SAT_new)
+# Add test score data to student data
 students <- students %>% 
-  left_join(concordance_scores, by="hashed_puid")
+  left_join(students_test_scores_equated, by="hashed_puid")
 
-# Calculate the Pearson correlation between graduating GPA and new SAT score
-# Generate a scatter plot matrix for graduating GPA and new SAT score
-# cor.test(students_test_scores_equated$grad_1_overall_gpa, students_test_scores_equated$SAT_new)
-# data.gpa <- students_test_scores_equated %>% 
-#   select(grad_1_overall_gpa, SAT_new) %>% 
-#   chart.Correlation(method = "pearson", histogram = TRUE, pch = 16)
-
-# Exploring missing test scores -------------------------------------------
-
-# Filter for students who do not have either ACT or SAT scores
-test_missing <- students[is.na(students$SAT_new),]
-table(test_missing$profile_residence_desc)
-table(test_missing$graduated_flag)
-
-# Remove international students because they are not required to do these tests
-test_missing_local <- filter(test_missing, profile_residence_desc != "Foreign")
-table(test_missing_local$profile_admissions_pop_desc)
-table(test_missing_local$graduated_flag)
-
-# Focus on new students who should have test scores
-test_missing_new <- filter(test_missing_local, profile_admissions_pop_desc == "New First Time")
-table(test_missing_new$graduated_flag)
