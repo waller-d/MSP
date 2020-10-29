ae3f6eb17dc3fab714d9079c9042f64e935bfe68
waller-d <28006978+waller-d@users.noreply.github.com>
2020-10-29 11:41:24 -0400

    Creating scripts to calculate representation metrics at the college level and in engineering.

--- representation-metrics.R
+++ representation-metrics.R
@@ -8,6 +8,56 @@
 library(tidyverse)
 
 
+# Engineering population breakdown ----------------------------------------
+
+eng_studens <- students %>% 
+  filter(academic_school_grouping_desc == "College of Engineering") %>% 
+  group_by(profile_reporting_ethnicity) %>% 
+  summarise(N = n()) %>% 
+  mutate(percent = N/21993)
+
+eng_students_ethnicity <- students %>% 
+  filter(academic_school_grouping_desc == "College of Engineering") %>% 
+  group_by(profile_reporting_ethnicity, department_short_title) %>% 
+  summarise(N = n())
+
+eng_students_department <- students %>% 
+  filter(academic_school_grouping_desc == "College of Engineering") %>% 
+  group_by(department_short_title, profile_reporting_ethnicity) %>% 
+  summarise(N = n()) %>% 
+  mutate(percent = N/sum(N))
+
+eng_students_department %>% 
+  filter(department_short_title == "Aeronautics & Astronautics") %>% 
+  summarise(sum(percent))
+
+eng_students_cohort <- students %>% 
+  filter(academic_school_grouping_desc == "College of Engineering") %>% 
+  group_by(profile_academic_period, department_short_title, profile_reporting_ethnicity) %>% 
+  summarise(N = n()) %>% 
+  mutate(percent = N/sum(N))
+
+ggplot(eng_students_ethnicity, aes(x = N, y = profile_reporting_ethnicity)) +
+  geom_segment(aes(x = 0, y = profile_reporting_ethnicity, xend = N, yend = profile_reporting_ethnicity)) +
+  geom_point() + facet_grid(department_short_title ~ .)
+
+eng_students_cohort_hispanic_AA <- students %>% 
+  filter(academic_school_grouping_desc == "College of Engineering") %>% 
+  filter(profile_reporting_ethnicity == "Hispanic/Latino" | profile_reporting_ethnicity == "Black or African American" | profile_reporting_ethnicity == "White" | profile_reporting_ethnicity == "Asian" ) %>% 
+  filter(Term == "Fall") %>% 
+  group_by(profile_academic_period, profile_reporting_ethnicity) %>% 
+  summarise(N = n()) #%>% 
+mutate(percent = N/sum(N))
+
+ggplot(eng_students_cohort_hispanic_AA, aes(x = profile_academic_period, y = N)) +
+  geom_point(aes(color = profile_reporting_ethnicity)) +
+  geom_line(aes(color = profile_reporting_ethnicity))
+
+ggplot(eng_students_cohort_hispanic_AA, aes(fill = profile_reporting_ethnicity, y = N, x = profile_academic_period)) +
+         geom_bar(position = "fill", stat = "identity")
+
+
+
 # College representation --------------------------------------------------
 
 # Calculate the total number enrolled in each college across all years
@@ -78,6 +128,24 @@ major_enrollment_URM <- students %>%
   left_join(., major_enrollment, by = "department_short_title") %>% 
   mutate(percent = N.x/N.y)
 
+# Calculate the total number enrolled in each major across all years by URM status
+# Calculate the percentage of URM in each engineering major
+major_enrollment_URM_engineering <- students %>% 
+  filter(academic_school_grouping_desc == "College of Engineering") %>% 
+  group_by(department_short_title, URM) %>% 
+  summarise(N = n()) %>% 
+  left_join(., major_enrollment, by = "department_short_title") %>% 
+  mutate(percent = N.x/N.y)
+
+# Calculate the total number enrolled in each major across all years by gender status
+# Calculate the percentage of URM in each engineering major
+major_enrollment_gender_engineering <- students %>% 
+  filter(academic_school_grouping_desc == "College of Engineering") %>% 
+  group_by(department_short_title, profile_gender_desc) %>% 
+  summarise(N = n()) %>% 
+  left_join(., major_enrollment, by = "department_short_title") %>% 
+  mutate(percent = N.x/N.y)
+
 # Calculate the total number enrolled in each major for each cohort
 major_enrollment_cohort <- students %>% 
   group_by(department_short_title, profile_academic_period_desc) %>% 
@@ -102,3 +170,38 @@ major_enrollment_cohort_URM <- students %>%
   left_join(., major_enrollment_cohort, by = "major_year") %>% 
   mutate(percent = N.x/N.y)
 
+
+
+# Gender and URM representation in engineering ----------------------------
+
+x <- students %>% 
+  filter(academic_school_grouping_desc =="College of Engineering") %>% 
+  group_by(department_short_title, Term, Year, URM) %>% 
+  summarise(N = n()) %>% 
+  mutate(major_year = paste(department_short_title, Term, Year)) %>% 
+  left_join(., major_enrollment_cohort, by = "major_year") %>% 
+  mutate(percent = N.x/N.y) %>% 
+  filter(Term == "Fall", URM == TRUE)
+
+x
+
+ggplot(data = x, aes(x = Year, y = percent, group = department_short_title.x)) +
+  geom_line(aes(color = department_short_title.x)) +
+  geom_point(aes(color = department_short_title.x)) +
+  facet_grid(department_short_title.x ~ .)
+
+y <- students %>% 
+  filter(academic_school_grouping_desc =="College of Engineering") %>% 
+  group_by(department_short_title, Term, Year, profile_gender_desc) %>% 
+  summarise(N = n()) %>% 
+  mutate(major_year = paste(department_short_title, Term, Year)) %>% 
+  left_join(., major_enrollment_cohort, by = "major_year") %>% 
+  mutate(percent = N.x/N.y) %>% 
+  filter(Term == "Fall", profile_gender_desc == "Female")
+
+y
+
+ggplot(data = y, aes(x = Year, y = percent, group = department_short_title.x)) +
+  geom_line(aes(color = department_short_title.x)) +
+  geom_point(aes(color = department_short_title.x)) +
+  facet_grid(department_short_title.x ~ .)
